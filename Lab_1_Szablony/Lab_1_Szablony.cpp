ee5baa2590017257899ec964c51dc5015098a2f0
NikodemWspanialy <91847123+NikodemWspanialy@users.noreply.github.com>
2022-10-18 13:32:45 +0200

    Lab1Szablony

---
+++ Lab_1_Szablony/Lab_1_Szablony.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Vector.h"
+
+int main()
+{
+    std::string s = "a";
+    nw::Vector<int> v(10);
+    for (size_t i = 0; i < v.Size(); i++) {
+        std::cout << v[i] << std::endl;
+    }
+}
