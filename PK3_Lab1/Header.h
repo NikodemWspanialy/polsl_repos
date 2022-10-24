7a987cd5a74e5c8336dd07033c6fc84987869d56
NikodemWspanialy <91847123+NikodemWspanialy@users.noreply.github.com>
2022-10-24 19:23:27 +0200

    Lab_1

---
+++ PK3_Lab1/Header.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <string>
+#include<functional>
+#include <iostream>
+namespace nw {
+	template<class T> T DefValue() { std::cout << "unknow type"; throw std::exception(); }
+	template<> double DefValue() { return 0.0; }
+	template<> int DefValue() { return 0; }
+	template<> float DefValue() { return 0; }
+	template<> char DefValue() { return ' '; }
+	template<> std::string DefValue() { return ""; }
+	template <class T>
+	class Vector
+	{
+		size_t size;
+		T* data;
+	public:
+		Vector(const size_t size = 0, const T& defValue = DefValue<T>()) : size(size), data(new T[size]) {
+			for (size_t i = 0; i < size; ++i) {
+				data[i] = defValue;
+			}
+		}
+		Vector(const size_t size, const std::function<T(const size_t)>& f) : size(size), data(new T[size]) {
+			for (size_t i = 0; i < size; ++i) {
+				data[i] = f(i);
+			}
+		}
+		Vector(Vector& v) noexcept :size(v.sizeGetter()), data(v.dataGetter()) {
+			for (size_t i = 0; i < size; i++)
+				data[i] = v.data[i];
+		}
+		Vector(Vector&& v) noexcept :size(v.sizeGetter()), data(v.dataGetter()) {
+			v.data = 0;
+			v.data = nullptr;
+		}
+
+		~Vector() noexcept {
+			delete[] data;
+		}
+		T& operator[](const size_t index) const {
+			return data[index];
+		}
+		T& operator[](const size_t index) {
+			return data[index];
+		}
+		size_t sizeGetter() const noexcept {
+			return size;
+		}
+		T* dataGetter() {
+			return data;
+		}
+		 void pushback(const T& value) noexcept{
+			++size;
+			T* datatmp;
+			datatmp = new T[size];
+			for (size_t i = 0; i < size - 1; i++) {
+				datatmp[i] = data[i];
+			}
+			datatmp[size-1] = value;
+			data = nullptr;
+			data = datatmp;
+		}
+	    bool operator ==(Vector<T> &b) const {
+			if (size != b.sizeGetter())
+				return false;
+			if (this->size < b.sizeGetter()) {
+				for (int i = 0; i < this->size; i++) {
+					if (data[i] != b[i])
+						return false;
+				}
+				return true;
+			}
+			else
+			{
+				for (int i = 0; i < b.sizeGetter(); i++) {
+					if (data[i] != b[i])
+						return false;
+				}
+				return true;
+			}
+		}
+		bool operator !=(Vector<T>& b) const {
+			if (size != b.sizeGetter())
+				return true;
+			if (this->size < b.sizeGetter()) {
+				for (int i = 0; i < this->size; i++) {
+					if (data[i] != b[i])
+						return true;
+				}
+				return false;
+			}
+			else
+			{
+				for (int i = 0; i < b.sizeGetter(); i++) {
+					if (data[i] != b[i])
+						return true;
+				}
+				return false;
+			}
+		}
+	};
+}
---
+++ PK3_Lab1/PK3_Lab1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "Header.h"
+#include<conio.h>
+#define LOG(x) std::cout << x << std::endl;
+int main()
+{
+    nw::Vector<char> v1;
+    v1.pushback('b');
+    v1.pushback('c');
+    for (int i = 0; i < v1.sizeGetter(); i++) {
+        std::cout << v1[i];
+    }
+    LOG("");
+    //potega 2
+    {
+        int a = 1;
+        nw::Vector<int> v2(10,[&a](int) mutable {a = 2 * a; return a; });
+        for (int i = 0; i < v2.sizeGetter(); i++) {
+            LOG(v2[i]);
+        }
+    }
+
+    //ciag fib
+    {
+        int a = 1, b = 1;
+        nw::Vector<int> vFib(10, [&](int)mutable {int a_tmp = a, b_tmp = b; a = a + b; b = a_tmp; return b_tmp; });
+        for (int i = 0; i < vFib.sizeGetter(); i++) {
+            LOG(vFib[i]);
+        }
+    }
+    _getch();
+}
